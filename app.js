61cc81b94b3bc0c9148ccf43dfee9295f76895ab
HolstAndreas <166335551+HolstAndreas@users.noreply.github.com>
2024-10-21 10:11:41 +0300

    config app.js as express instance

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
-// äpp peaks käivituma package.json scripti järgi, konsoolis:
-// npm run dev
-console.log("Hello World");
+// Imports everything necessary for app.js to function as an express instance.
+import express from "express"; // Import express module - handles routing, middleware, HTTP requests, responses
+import path from "path"; // Provides ulities for working with file and directory paths.
+import { fileURLToPath } from "url"; // converts a file URL into an actual filepath - in ES Node.js does not provide built-in __filename or __dirname...
+import { getRaceById } from "./controllers/raceController.js"; // import all methods from raceControllers.js
 
-import express from "express";
-import path from "path";
-import { fileURLToPath } from "url";
-import { getRaceById } from "./controllers/raceController.js";
+// Sets up Express
+const app = express(); // app is now an instance of Express application.
 
-const app = express();
+// Middleware Configuration:
+app.use(express.json()); // Parse JSON - adds middleware that parses JSON payloads - convert the body into a JS object available under req.body.
+app.use(express.urlencoded({ extended: true })); // Middleware to parse URL-encoded data from form submissions, true == allows rich objects and arrays.
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// File and Directory Setup:
+const __filename = fileURLToPath(import.meta.url); // Converts current URL of the module into a filepath using...
+const __dirname = path.dirname(__filename); // Directory name where __filepath is full path. Useful for relative path in relation to current dir.
+
+// Setting View Engine and Views Directory:
+app.set("views", path.join(__dirname, "views")); // Express will look for views in the views folder located in the same dir as current file.
+app.set("view engine", "ejs"); // Sets EJS as view engine.
+
+// Here should all defined routes be. (Endpoint, method)
+app.get("/races", getRaceById);
+
+// Starts the server
+const PORT = process.env.PORT || 3000; // Sets the port number, checks for environment variables, default is 3000.
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`); // Start listening to requests at PORT
+});
